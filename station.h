2152e68363552923efbc1d0d0cf934db0387d9f0
Gordon JC Pearce <gordon@gjcp.net>
2011-06-16 07:47:26 +0100

    station gobject

--- station.h
+++ station.h
@@ -6,12 +6,26 @@
 #include <gtk/gtk.h>
 #include <osm-gps-map.h>
 
+// GObject macros
+#define APRSMAP_TYPE_STATION                  (aprsmap_station_get_type ())
+#define APRSMAP_STATION(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), APRSMAP_TYPE_STATION, APRSMapStation))
+#define APRSMAP_IS_STATION(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), APRSMAP_TYPE_STATION))
+#define APRSMAP_STATION_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), APRSMAP_TYPE_STATION, APRSMapStationClass))
+#define APRSMAP_IS_STATION_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), APRSMAP_TYPE_STATION))
+#define APRSMAP_STATION_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), APRSMAP_TYPE_STATION, APRSMAPStationClass))
+
+typedef struct _APRSMapStation APRSMapStation;
+typedef struct _APRSMapStationClass APRSMapStationClass;
+
+
+
 typedef enum {
 	APRS_NOFIX,
 	APRS_VALIDFIX
 } aprs_fix_t;
 
-typedef struct {
+struct _APRSMapStation {
+	GObject parent_instance;
 	gchar *callsign;
 	gchar symbol[2];
 	OsmGpsMapImage *image;
@@ -22,8 +36,12 @@ typedef struct {
 	gdouble course;
 	gdouble lat, lon;
 	aprs_fix_t fix;
-} APRSMapStation;
+};
 
+struct _APRSMapStationClass {
+	GObjectClass parent_class;
+};
+GType aprsmap_station_get_type (void);
 gboolean process_packet(gchar *msg);
 
 #endif
